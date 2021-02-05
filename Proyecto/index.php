e00c94e71375894473bf04d1ed0dc18ae99523d4
WilmerAlquinga <78151769+WilmerAlquinga@users.noreply.github.com>
2021-02-05 01:47:25 -0500

    Create index.php

---
+++ Proyecto/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel=StyleSheet href="css/estilos.css" TYPE="text/css" />
+    <title>Proyecto - G3</title>
+</head>
+<body>
+    <br>
+    <div style="position: relative; width:100%; height:100%;">
+        <h1 style="text-align:center; color:white">Proyecto Grupo 3</h1>
+        <br>
+        <div class="cont1">
+            <br><br>
+            <h2>Acceso al sistema</h2>
+            <form action="Usuario/registro.php">
+                <br>
+                <label for="fname">Nombre de usuario:</label><br>
+                <input type="text" id="fname" name="fname"><br>
+                <br>
+                <label for="lname">Contraseña:</label><br>
+                <input type="text" id="lname" name="lname"><br><br>
+                <br>
+                <input type="submit" value="Acceder">
+                <br><br><br>
+                <input type="submit" value="Registrarse">
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
