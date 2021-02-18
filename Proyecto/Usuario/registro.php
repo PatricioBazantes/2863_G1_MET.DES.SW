7145134935a149af4105389eafabb382f03d65e8
WilmerAlquinga <78151769+WilmerAlquinga@users.noreply.github.com>
2021-02-18 08:48:40 -0500

    Update registro.php

--- Proyecto/Usuario/registro.php
+++ Proyecto/Usuario/registro.php
@@ -16,19 +16,19 @@
             <form action="../bd/conexion.php" method="POST">
                 <br>
                 <label for="fname">Nombre de usuario:</label><br>
-                <input type="text" id="user" name="user" required><br>
+                <input type="text" id="user" name="user" required pattern="[A-Za-z0-9]+"><br>
                 <br>
                 <label for="fname">Nombre:</label><br>
-                <input type="text" id="fname" name="fname" required><br>
+                <input type="text" id="fname" name="fname" required pattern="[A-Za-z]+"><br>
                 <br>
                 <label for="fname">Apellido:</label><br>
-                <input type="text" id="lname" name="lname" required><br>
+                <input type="text" id="lname" name="lname" required pattern="[A-Za-z]+"><br>
                 <br>
                 <label for="fname">Correo:</label><br>
                 <input type="email" id="mail" name="mail" required><br>
                 <br>
                 <label for="lname">Contraseña:</label><br>
-                <input type="password" id="pass" name="pass" required><br>
+                <input type="password" id="pass" name="pass" required minlength="3"><br>
                 <br>
                 <input type="submit" value="Enviar">
                 <br>
